5b97351f2608581eafdb8d99fc6a997534e4e663
FelipeN21 <felipenepomuceno21@hotmail.com>
2022-02-26 15:24:40 -0300

    Alteracoes basicas

--- Generics.cs
+++ Generics.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AtivSem02
 {
-    internal class Generics
+    public class Generics
     {
 
         public class Alunos
@@ -19,49 +19,61 @@ namespace AtivSem02
 
 
             }
+
+            public Alunos(string nome,int idade, int notaFinal)
+            {
+                this.nome = nome;
+                this.idade = idade;
+                this.notaFinal = notaFinal;
+
+
+            }
             public string nome { get; set; }
             public int idade { get; set; }
             public int notaFinal { get; set; }
 
 
-            public static List<object> NomeQ()
-            {
-                List<object> list = new List<object>();
-                Alunos A = new Alunos();
-                list.Add(A);
-
-
-                return list;
-            }
-
-            public static List<T> Funcao<T>()
-            {
-                List<T> list = new List<T>();
-                List<Alunos> alunos = new List<Alunos>();
-                T A1, A2, A3, A4, A5, A6, A7, A8, A9, A10;
-
-                alunos.Add(new Alunos { nome = "Felipe" , idade = 21, notaFinal = 20});
-                alunos.Add(new Alunos { nome = "Felipe2", idade = 21, notaFinal = 20 });
-                alunos.Add(new Alunos { nome = "Felipe3", idade = 21, notaFinal = 20 });
-                alunos.Add(new Alunos { nome = "Felipe4", idade = 21, notaFinal = 20 });
-                alunos.Add(new Alunos { nome = "Felipe5", idade = 21, notaFinal = 20 });
-
-                /*list.Add(new typeof(A1));
-                list.Add(A2);
-                list.Add(A3);
-                list.Add(A4);
-                list.Add(A5);
-                list.Add(A6);
-                list.Add(A7);
-                list.Add(A8);
-                list.Add(A9);
-                list.Add(A10);*/
-
-
-
-                return list;
-            }
+       
 
         }
+
+        public static List<T> Funcao<T>(T[] alunoQualquer)
+        {
+            List<T> list = new List<T>();
+            for(int i = 0;i < 10 ;i++ )
+            { 
+            list.Add(alunoQualquer[i]);
+            }
+          
+
+
+
+            return list;
+        }
+
+        public static string BuscarAlunoPeloNome<Alunos>(List<Generics.Alunos> listaDosAlunos,string nomeBuscado )
+        {
+
+         
+
+            var filtro =  listaDosAlunos.Where(a => a.nome.Equals(nomeBuscado)).Select(a=>a);
+
+            
+            Generics.Alunos[] array = filtro.ToArray();
+            
+            Console.WriteLine(array.GetType().Name);
+            foreach (Generics.Alunos listado in array)
+            {
+                Console.WriteLine ("\nNome : " + listado.nome +"\nIdade : "+ listado.idade +"\nNota Final : "+listado.notaFinal+ "\n");
+            }
+
+           
+
+
+            return "Hey";
+        }
+
     }
+
+
 }
--- InjDep.cs
+++ InjDep.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AtivSem02;
 
 namespace AtivSem02
 {
@@ -12,6 +13,30 @@ namespace AtivSem02
         
         public static void Main(string[] args)
         {
+
+
+            //Tarefa 2 -------
+            Generics.Alunos Teste1 = new Generics.Alunos();
+            Generics.Alunos[] arrayDeAlunos = { 
+                new Generics.Alunos("Rodrigo", 20, 99),
+                new Generics.Alunos("Felipe", 19, 50), 
+                new Generics.Alunos("Rodrigo", 18, 65), 
+                new Generics.Alunos("Leticia", 28, 62), 
+                new Generics.Alunos("Luana", 36, 61),
+                new Generics.Alunos("Gabriela",40,60),
+            new Generics.Alunos("Fernando",30,55),
+            new Generics.Alunos("Pedro",50,55),
+            new Generics.Alunos("Igor",99,77),
+            new Generics.Alunos("Laura",11,88)};
+
+            
+            List<Generics.Alunos> listaAlunos = Generics.Funcao<Generics.Alunos>(arrayDeAlunos);
+            Generics.BuscarAlunoPeloNome<Generics.Alunos>(Generics.Funcao<Generics.Alunos>(arrayDeAlunos), "Luana");
+
+
+
+
+            //Tarefa 3 -------
             TransferenciasBancarias Teste = new TransferenciasBancarias(new BancosDaTransferencias.Banco01(),new BancosDaTransferencias.Banco02());
             Teste.Transferir(50);
             Console.ReadKey();
