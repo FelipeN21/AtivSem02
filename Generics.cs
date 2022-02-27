bc7eb6015da194f280663a55c0302eab9cf8773d
FelipeN21 <felipenepomuceno21@hotmail.com>
2022-02-27 12:55:04 -0300

    Atualizacao com projeto Web

--- Generics.cs
+++ Generics.cs
@@ -11,6 +11,9 @@ namespace AtivSem02
 
         public class Alunos
         {
+            /// <summary>
+            /// Construtor sem parametragem
+            /// </summary>
             public Alunos()
             {
                 this.nome = "AlunoQualquer";
@@ -19,7 +22,12 @@ namespace AtivSem02
 
 
             }
-
+            /// <summary>
+            /// Construtor parametrado com todos atributos
+            /// </summary>
+            /// <param name="nome">Nome do aluno</param>
+            /// <param name="idade">Idade do aluno</param>
+            /// <param name="notaFinal">Nota Final do aluno</param>
             public Alunos(string nome,int idade, int notaFinal)
             {
                 this.nome = nome;
@@ -37,6 +45,12 @@ namespace AtivSem02
 
         }
 
+        /// <summary>
+        /// Preenche a lista com os alunos passados
+        /// </summary>
+        /// <typeparam name="T">Tipo generico da lista</typeparam>
+        /// <param name="alunoQualquer">Array de alunos a serem adicionados na lista</param>
+        /// <returns></returns>
         public static List<T> Funcao<T>(T[] alunoQualquer)
         {
             List<T> list = new List<T>();
@@ -51,26 +65,44 @@ namespace AtivSem02
             return list;
         }
 
-        public static string BuscarAlunoPeloNome<Alunos>(List<Generics.Alunos> listaDosAlunos,string nomeBuscado )
+
+        /// <summary>
+        /// Funcao que recebe a lista generica e pesquisa alunos na mesma
+        /// </summary>
+        /// <typeparam name="Alunos"></typeparam>
+        /// <param name="listaDosAlunos">Lista generica a ser analisada</param>
+        /// <param name="nomeBuscado">Nome o qual deve ser buscado na lista</param>
+        /// <returns></returns>
+        public static Generics.Alunos BuscarAlunoPeloNome<Alunos>(List<Generics.Alunos> listaDosAlunos,string nomeBuscado )
         {
 
-         
-
-            var filtro =  listaDosAlunos.Where(a => a.nome.Equals(nomeBuscado)).Select(a=>a);
-
-            
-            Generics.Alunos[] array = filtro.ToArray();
-            
-            Console.WriteLine(array.GetType().Name);
-            foreach (Generics.Alunos listado in array)
+            try
             {
-                Console.WriteLine ("\nNome : " + listado.nome +"\nIdade : "+ listado.idade +"\nNota Final : "+listado.notaFinal+ "\n");
+
+                var filtro = listaDosAlunos.Where(a => a.nome.Equals(nomeBuscado)).Select(a => a);
+
+
+                Generics.Alunos[] array = filtro.ToArray();
+
+                Console.WriteLine(array.GetType().Name);
+                Generics.Alunos Salvar = new Generics.Alunos();
+                foreach (Generics.Alunos listado in array)
+                {
+                    Console.WriteLine("\nNome : " + listado.nome + "\nIdade : " + listado.idade + "\nNota Final : " + listado.notaFinal + "\n");
+                    Salvar = listado;
+                }
+
+
+
+
+                return Salvar;
             }
-
-           
-
-
-            return "Hey";
+            catch (Exception ex) { 
+                Console.WriteLine("Aluno escolhido não encontrado na lista Ou lista Incompativel");
+            Console.Write(ex.Message);
+                Generics.Alunos Erro = new Generics.Alunos {nome = "ERRO : ALUNO NAO EXISTENTE" };
+                return Erro;
+            }
         }
 
     }
--- InjDep.cs
+++ InjDep.cs
@@ -18,12 +18,12 @@ namespace AtivSem02
             //Tarefa 2 -------
             Generics.Alunos Teste1 = new Generics.Alunos();
             Generics.Alunos[] arrayDeAlunos = { 
-                new Generics.Alunos("Rodrigo", 20, 99),
-                new Generics.Alunos("Felipe", 19, 50), 
-                new Generics.Alunos("Rodrigo", 18, 65), 
-                new Generics.Alunos("Leticia", 28, 62), 
-                new Generics.Alunos("Luana", 36, 61),
-                new Generics.Alunos("Gabriela",40,60),
+            new Generics.Alunos("Rodrigo", 20, 99),
+            new Generics.Alunos("Felipe", 19, 50), 
+            new Generics.Alunos("Rodrigo", 18, 65), 
+            new Generics.Alunos("Leticia", 28, 62), 
+            new Generics.Alunos("Luana", 36, 61),
+            new Generics.Alunos("Gabriela",40,60),
             new Generics.Alunos("Fernando",30,55),
             new Generics.Alunos("Pedro",50,55),
             new Generics.Alunos("Igor",99,77),
@@ -31,7 +31,10 @@ namespace AtivSem02
 
             
             List<Generics.Alunos> listaAlunos = Generics.Funcao<Generics.Alunos>(arrayDeAlunos);
-            Generics.BuscarAlunoPeloNome<Generics.Alunos>(Generics.Funcao<Generics.Alunos>(arrayDeAlunos), "Luana");
+            //Pode-se mudar o ultimo parametro <Nome> para poder procurar outros alunos
+            //Detalhe Importante -> Seria possivel retornar mais de um aluno com mesmo nome por meio de um array
+            //Mas dadas as instrucoes do exercicio so sera retornado 1.
+            Generics.BuscarAlunoPeloNome<Generics.Alunos>(Generics.Funcao<Generics.Alunos>(arrayDeAlunos), "Rodrigo");
 
 
 
